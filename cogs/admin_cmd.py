1341239791da0d9eedbcaf3f63094fac42ba9b55
Thomas Van den Steen <thomas-vds@live.be>
2020-10-14 23:42:56 +0200

    Add pseudo ban command

--- cogs/admin_cmd.py
+++ cogs/admin_cmd.py
@@ -1,11 +1,17 @@
 import discord
 from discord.ext import commands
+import logging
 
 # Contains commands useful to server administration
 class AdminCmd(commands.Cog):
     # Initializes the cog
     def __init__(self, bot):
         self.bot = bot;
+
+    @commands.command()
+    async def ban(self,ctx):
+        await ctx.send("Haha, you're banned")
+        logging.debug("One less retard")
 
 # setup command for the cog
 def setup(bot):
