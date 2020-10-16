01df78bbdb0ef18276f26becb0e5195d31a8f051
Thomas Van den Steen <thomas-vds@live.be>
2020-10-16 20:06:00 +0200

    Fix purge command

--- cogs/admin_cmd.py
+++ cogs/admin_cmd.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import logging
+import typing
 
 # Contains commands useful to server administration
 class AdminCmd(commands.Cog):
@@ -29,16 +30,37 @@ class AdminCmd(commands.Cog):
             await ctx.send("You did not pass all the required arguments for this command.")
         else: 
             pass #TODO logger.debug("unknown error when running ban command: "+ error)
-        
+
     @commands.command()
     @commands.has_permissions(manage_messages = True)
-    async def purge(self,ctx,target: commands.MemberConverter,amount: int):
+    async def purge(self,ctx,target: typing.Optional[discord.Member] = None, limit: int = 0):
+        """
+        target: The member who's messages you want to delete (optional)
+        limit: the amount of messages to search through
+        """
         try:
-            channel = ctx.message.channel
-            def is_target(m):
-                return m.author == target
-            deleted = await channel.purge(limit = amount,check = is_target)
-            await channel.send("Deleted {} message(s)".format(len(deleted)))
+            # Delete the message that calls this command
+            await ctx.message.delete()
+
+            msg = []
+            channel = ctx.channel
+
+            if target == None:
+                await ctx.channel.purge(limit=limit)
+                return await ctx.send(f"Purged {limit} messages", delete_after=3)
+            
+            if limit == 0: 
+                return await ctx.send("You did not specify the amount.", delete_after=5)
+            
+            async for m in channel.history():
+                if len(msg) == limit:
+                    # End of loop condition
+                    break
+                if m.author == target:
+                    # Keep adding messages authored by this member until end of loop condition
+                    msg.append(m)
+            await ctx.channel.delete_messages(msg)
+            await ctx.send(f"Deleted {limit} message(s) of {target.mention}", delete_after=3)
             #TODO logger.debug("Successfully deleted {} message(s) from {}".format(len(deleted),target)
         except discord.Forbidden as Forbidden:
             await ctx.send("You do not have permissions to do the actions required.")
