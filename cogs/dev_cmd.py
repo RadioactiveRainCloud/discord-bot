5f66b7e5fc1f0c9113dc944b378d643f9cd61701
Callum Gray <cgray95@gmail.com>
2020-10-17 00:14:53 +1000

    truth or dare module

--- cogs/dev_cmd.py
+++ cogs/dev_cmd.py
@@ -1,11 +1,12 @@
 import discord
 from discord.ext import commands
 
+
 # Cog used for reloading updated cogs while bot is running
 class DevCmd(commands.Cog):
     # Initializes the cog.
     def __init__(self, bot):
-        self.bot = bot;
+        self.bot = bot
 
     # Command used to trigger a reload. The argument list should include the
     # names of the extensions to reload.
@@ -13,89 +14,90 @@ class DevCmd(commands.Cog):
     @commands.is_owner()
     async def reload(self, ctx, *args):
         # Generate a message to be sent after completion
-        message = "Reload called.\n";
+        message = "Reload called.\n"
         # Iterate through arguments and reload them if they exist
         for arg in args:
             # Set up a variable to store the extension name.
-            ext = None;
+            ext = None
             # Search extension list for the argument
             for extension in self.bot._extension_list:
                 if extension.endswith(arg):
-                    ext = extension;
-                    break;
-            if ext == None:
-                message += f"{arg} does not exist in loaded extensions.\n";
+                    ext = extension
+                    break
+            if ext is None:
+                message += f"{arg} does not exist in loaded extensions.\n"
             else:
                 message += f"Reloading {ext}.\n"
                 # Try to reload the extension; add result to message
                 if ext in self.bot._extension_list:
                     try:
-                        self.bot.reload_extension(ext);
+                        self.bot.reload_extension(ext)
                     except commands.errors.ExtensionNotLoaded:
-                        message += f"{ext} not loaded.\n";
+                        message += f"{ext} not loaded.\n"
                     except commands.errors.ExtensionNotFound:
-                        message += f"{ext} not found.\n";
+                        message += f"{ext} not found.\n"
                     except commands.errors.ExtensionFailed as e:
-                        print(e);
-                        message += f"{ext} failed; execution error.\n";
+                        print(e)
+                        message += f"{ext} failed; execution error.\n"
                     except commands.errors.NoEntryPointError:
-                        message += f"{ext} has no setup function.\n";
+                        message += f"{ext} has no setup function.\n"
                     else:
-                        message += f"{ext} reloaded.\n";
+                        message += f"{ext} reloaded.\n"
                 else:
-                    message += f"{ext} is not in the extension list.\n";
+                    message += f"{ext} is not in the extension list.\n"
         # Remove trailing newline and notify the user
-        message = message[:-1];
-        print(message);
-        await ctx.send(message);
+        message = message[:-1]
+        print(message)
+        await ctx.send(message)
 
     # Reloads all extensions in bot's extension list.
     @commands.command()
     @commands.is_owner()
     async def reload_all(self, ctx):
         # Generate a message to be sent after completion
-        message = "Reloading all extensions. . .\n";
+        message = "Reloading all extensions. . .\n"
         # Iterate through all extensions in bot's extension list and reload them
         for ext in self.bot._extension_list:
             try:
-                self.bot.reload_extension(ext);
+                self.bot.reload_extension(ext)
             except commands.errors.ExtensionNotLoaded:
-                message += f"{ext} not loaded.\n";
+                message += f"{ext} not loaded.\n"
             except commands.errors.ExtensionNotFound:
-                message += f"{ext} not found.\n";
+                message += f"{ext} not found.\n"
             except commands.errors.ExtensionFailed:
-                message += f"{ext} failed; execution error.\n";
+                message += f"{ext} failed; execution error.\n"
             except commands.errors.NoEntryPointError:
-                message += f"{ext} has no setup function.\n";
+                message += f"{ext} has no setup function.\n"
             else:
-                message += f"{ext} reloaded.\n";
-        message = message[:-1]; #get rid of trailing newline
+                message += f"{ext} reloaded.\n"
+        message = message[:-1]  # get rid of trailing newline
         # Notify the user
-        await ctx.send(message);
-        print(message);
+        await ctx.send(message)
+        print(message)
 
     # Resets all extensions, rebuilding the extension list from scratch
     @commands.command()
     @commands.is_owner()
     async def reset_all(self, ctx):
-        message = "Begin reset of all extensions.\nUnloading extensions. . .\n";
+        message = "Begin reset of all extensions.\nUnloading extensions. . .\n"
         for ext in self.bot._extension_list:
             try:
-                self.bot.unload_extension(ext);
+                self.bot.unload_extension(ext)
             except commands.errors.ExtensionNotLoaded:
-                message += f"{ext} was not loaded\n";
+                message += f"{ext} was not loaded\n"
             else:
-                message += f"{ext} unloaded successfully\n";
+                message += f"{ext} unloaded successfully\n"
         # Rebuild the extension list from scratch and load the extensions from
         # the generated extension list.
 
-        message += "Rebuilding extension list from scratch. . .\n";
-        message += self.bot._build_extensions();
+        message += "Rebuilding extension list from scratch. . .\n"
+        message += self.bot._build_extensions()
 
         # Notify the user
-        print(message);
-        await ctx.send(message);
+        print(message)
+        await ctx.send(message)
+
 
 # setup command for the cog
 def setup(bot):
-    bot.add_cog(DevCmd(bot));
+    bot.add_cog(DevCmd(bot))
--- main.py
+++ main.py
@@ -8,92 +8,98 @@ from dotenv import load_dotenv
 from discord.ext import commands
 from pathlib import Path
 
+
 class DiscordBotError(Exception):
-    pass;
+    pass
+
+
 class TokenError(DiscordBotError):
     def __init__(self):
-        super().__init__("No DISCORD_TOKEN defined in .env file.");
+        super().__init__("No DISCORD_TOKEN defined in .env file.")
+
+
 class DotEnvError(DiscordBotError):
     def __init__(self):
         super().__init__(
             ".env file not detected. Look in README.md for instructions on "
             "setting up the environment variables for this discord bot."
-        );
+        )
+
 
 # The bot's class definition
 class DiscordBot(commands.Bot):
-    def __init__(self, command_prefix = "$", embed_color = 0x3498db):
-        print("Initializing Discord bot. . .");
-        super().__init__(command_prefix);
-        self.embed_color = embed_color;
-        self.extension_list = self._build_extension_list();
-        self._load_extensions(self.extension_list);
-        print("Ready for start.");
+    def __init__(self, command_prefix="$", embed_color=0x3498db):
+        print("Initializing Discord bot. . .")
+        super().__init__(command_prefix)
+        self.embed_color = embed_color
+        self.extension_list = self._build_extension_list()
+        self._load_extensions(self.extension_list)
+        print("Ready for start.")
 
     # Builds the bot's extension list from the files in /cogs subdirectory.
     def _build_extension_list(self) -> List[str]:
-        extension_list = list();
-        folder_path = Path("cogs");
-        if folder_path.exists() == True:
+        extension_list = []
+        folder_path = Path("cogs")
+        if folder_path.exists():
             # for files ending in .py, format their names and add to the list.
             for item in folder_path.glob("**/*"):
                 if item.suffix == ".py":
-                    path = item.relative_to(folder_path.parent);
-                    ext = str(path).replace("\\", ".")[:-3];
-                    print(ext);
-                    extension_list.append(ext);
-                    print(f"added {ext} to cog list.");
+                    path = item.relative_to(folder_path.parent)
+                    ext = str(path).replace("\\", ".")[:-3]
+                    print(ext)
+                    extension_list.append(ext)
+                    print(f"added {ext} to cog list.")
         else:
-            warnings.warn("Warning: No cogs folder exists.", UserWarning);
+            warnings.warn("Warning: No cogs folder exists.", UserWarning)
         # Warn us if there were no cogs; bot will be useless.
         if not extension_list:
-            warnings.warn("Cogs folder has no .py extensions.", UserWarning);
+            warnings.warn("Cogs folder has no .py extensions.", UserWarning)
         return extension_list;
 
     # Iterates through each extension in extension list.
     def _load_extensions(self,extension_list: List[str]) -> None:
         for ext in extension_list:
             try:
-                self.load_extension(ext);
+                self.load_extension(ext)
             except commands.errors.ExtensionNotFound:
-                print(f"{ext} not found.");
+                print(f"{ext} not found.")
             except commands.errors.ExtensionAlreadyLoaded:
-                print(f"{ext} already loaded.");
+                print(f"{ext} already loaded.")
             except commands.errors.ExtensionFailed:
-                print(f"{ext} failed; execution error.");
+                print(f"{ext} failed; execution error.")
             except commands.errors.NoEntryPointError:
-                print(f"{ext} has not setup function.");
+                print(f"{ext} has not setup function.")
             else:
-                print(f"{ext} loaded successfully.");
+                print(f"{ext} loaded successfully.")
         return;
 
     # Loads environment variables and returns the discord token.
     def _load_token(self) -> str:
         # Load token from .env file. Raise error if no token in .env file.
         try:
-            if load_dotenv() == False:
-                raise DotEnvError;
-            token = os.getenv("DISCORD_TOKEN", default = None);
-            print("Loaded environment variables from .env");
-            if token == None:
-                raise TokenError;
-            print("Discord token found in environment variables.");
-            return token;
+            if not load_dotenv():
+                raise DotEnvError
+            token = os.getenv("DISCORD_TOKEN")
+            print("Loaded environment variables from .env")
+            if token is None:
+                raise TokenError
+            print("Discord token found in environment variables.")
+            return token
         except (TokenError, DotEnvError) as e:
-            print(e);
-            sys.exit();
+            print(e)
+            sys.exit()
 
     # Command to run the bot.
     def run(self):
-        super().run(self._load_token());
+        super().run(self._load_token())
 
     # Reports connection to discord.
     async def on_connect(self):
-        print("Successfully connected to discord.");
+        print("Successfully connected to discord.")
 
     # Reports that bot made it online.
     async def on_ready(self):
-        print(f"Logged in as {self.user}.");
+        print(f"Logged in as {self.user}.")
 
 
 discord_bot = DiscordBot();
